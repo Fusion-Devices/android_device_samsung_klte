a55a6e6a9de2dd4de50026e38b5c07e2264ed97e
Dragon <dragon7780@hotmail.nl>
2014-11-24 09:15:01 +0100

    Make it a fusion device
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

---
+++ fusionsp.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, vendor/fusion/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/fusion/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/samsung/klte/full_klte.mk)
+
+PRODUCT_DEVICE := klte
+PRODUCT_NAME := fusionsp_klte
