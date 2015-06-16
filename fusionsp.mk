cca4c4442957bca2f652ab0265db53aecc49e1b5
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-06-16 15:43:26 +0200

    Update fusion make file.
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- fusionsp.mk
+++ fusionsp.mk
@@ -5,4 +5,4 @@ $(call inherit-product, vendor/fusion/config/nfc_enhanced.mk)
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
 PRODUCT_DEVICE := klte
-PRODUCT_NAME := fusionsp_klte
+PRODUCT_NAME := klte
